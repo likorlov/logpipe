4cf206083eecacc3d171707dce2e96afc61a06b6
Lev Ikorlov <likorlov@users.noreply.github.com>
2026-04-19 09:46:54 +0800

    test: add example demonstrating batch flush on capacity

--- sink/batch_example_test.go
+++ sink/batch_example_test.go
@@ -24,3 +24,22 @@ func ExampleNewBatchSink() {
 	fmt.Println(received)
 	// Output: 2
 }
+
+func ExampleNewBatchSink_flushOnCapacity() {
+	var flushCount int
+	flushFn := func(batch []logpipe.Entry) error {
+		flushCount++
+		return nil
+	}
+
+	// batch size of 2 triggers flush after every 2 entries
+	s := sink.NewBatchSink(2, time.Second, flushFn)
+
+	_ = s.Write(logpipe.Entry{Message: "a"})
+	_ = s.Write(logpipe.Entry{Message: "b"}) // triggers flush
+	_ = s.Write(logpipe.Entry{Message: "c"})
+	_ = s.Close() // flushes remaining entry
+
+	fmt.Println(flushCount)
+	// Output: 2
+}
